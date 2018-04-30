4f891578211a05bdc2a034a75dc33512be4f2db7
Joana Palero <joana.palero24@gmail.com>
2018-04-30 00:56:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,21 @@
 # Your code goes here!
 class Dog
-
-def name=(name)
-@name = name
-end
-
-def name
-@name
-end
-end
-Willa = Dog.new
-willa.name = "Willa"
-
-willa.name
-
-willa.bark
-woof!
+  
+  def name=(name)
+  @name = name
+  end
+  
+  def name
+  @name
+  end
+  end
+  Willa = Dog.new
+  willa.name = "Willa"
+  
+  willa.name
+  
+  willa.bark
+  woof!
   
   
   
