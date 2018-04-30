328347a133c0704d0fe8b60511967158ba3bc01d
Joana Palero <joana.palero24@gmail.com>
2018-04-30 00:51:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,3 +6,18 @@ class Dog
     @breed = dogs_breed
   end
 end
+
+class Dog
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
+ 
+snoopy = Dog.new #=> #<Dog:0x007f970a2edfd0>
+snoopy.breed #=> nil
+snoopy.breed = "Beagle"
+snoopy.breed #=> "Beagle"
