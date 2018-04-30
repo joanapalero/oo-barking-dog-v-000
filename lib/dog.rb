3675a938108a80122da8b84d0f228f340ef81d54
Joana Palero <joana.palero24@gmail.com>
2018-04-30 00:53:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,9 @@
 # Your code goes here!
 class Dog
+  Willa = Dog.new
+  willa.name = "Willa"
   
-  def initialize(dogs_name, dogs_breed="Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-end
-
-class Dog
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
-end
- 
-snoopy = Dog.new #=> #<Dog:0x007f970a2edfd0>
-snoopy.breed #=> nil
-snoopy.breed = "Beagle"
-snoopy.breed #=> "Beagle"
+  willa.name
+  
+  willa.bark
+  woof!
